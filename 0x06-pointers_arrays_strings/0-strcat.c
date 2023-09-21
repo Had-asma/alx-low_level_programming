974d44d26e6ae1e9659cb097d0e2da1ca6686948
AsmaHad <asmahad25@gmail.com>
2023-09-21 16:19:32 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,24 @@
  *
  * Return: void
  */
-char *_strcat(char *dest, char *src){
-    int i;
-    int k;
-    i=0;
-    while ( dest[i]!='\0'){
-        i++;
-    }
-    k=0;
-    while(src[k] !='\0'){
-        dest[i]= src[k];
-        k++;
-        i++;
-    }
-     dest[i]='\0';
-    return( dest);
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
