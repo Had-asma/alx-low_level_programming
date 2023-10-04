fdb7735f359af0281b8307137a820556a58cd096
AsmaHad <asmahad25@gmail.com>
2023-10-04 05:47:32 -0700

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,9 @@
  * Return: 0 (Success)
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	(void)argv;
 	peintf("the number of arguments is :%d\n", argc - 1);
 
 	return (0);
