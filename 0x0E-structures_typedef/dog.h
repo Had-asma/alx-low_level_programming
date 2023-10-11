19b38d86c467f2d314b91841349065cde1b6dd95
AsmaHad <asmahad25@gmail.com>
2023-10-11 23:44:40 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog_t;
+
+#endif
