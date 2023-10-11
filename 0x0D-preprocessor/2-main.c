da0cb285f44139575293156f07ed640bfa09de88
AsmaHad <asmahad25@gmail.com>
2023-10-11 11:08:54 +0100

    Committing local changes

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+=======
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+>>>>>>> b23ce0fbddbf61c116164caa4b81a01b0b488fb3
